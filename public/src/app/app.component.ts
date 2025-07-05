eec0a6655ce0d2fd658ad241f3f0350745743757
Slavan-ST <slavan990900000@gmail.com>
2025-07-05 11:20:55 +0300

    fix

--- public/src/app/app.component.ts
+++ public/src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',          // Селектор для HTML
-  templateUrl: './app.component.html',  // Шаблон (HTML)
-  styleUrls: ['./app.component.css']    // Стили (CSS)
+  selector: 'app-root',
+  template: `
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div class="container">
+        <a class="navbar-brand" routerLink="/">Управление сотрудниками</a>
+        <div class="navbar-nav">
+          <a class="nav-link" routerLink="/about">О компании</a>
+          <a class="nav-link" routerLink="/employees">Сотрудники</a>
+        </div>
+      </div>
+    </nav>
+    <div class="container mt-4">
+      <router-outlet></router-outlet>
+    </div>
+  `
 })
-export class AppComponent {
-  title = 'Моё приложение';  // Пример переменной
-}
+export class AppComponent {}
--- public/src/app/app.module.ts
+++ public/src/app/app.module.ts
@@ -1,12 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
-import { CommonModule } from '@angular/common';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AboutCompanyComponent } from './components/about-company/about-company.component';
 import { EmployeesComponent } from './components/employees/employees.component';
@@ -21,13 +18,10 @@ import { EmployeeFormComponent } from './components/employee-form/employee-form.
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    HttpClientModule,
     FormsModule,
     ReactiveFormsModule,
-    RouterModule,
-    CommonModule,
-    NgbModule
+    HttpClientModule,
+    RouterModule.forRoot([])
   ],
   providers: [],
   bootstrap: [AppComponent]
